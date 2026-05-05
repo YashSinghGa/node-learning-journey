6e6e1c0615bc44c60b7e81bde241f998b28083e9
YashSinghGa <Yashgahlotsingh@gmail.com>
2026-05-05 13:06:27 +0530

    read json and created basic server

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("start learning"); 
+const http = require("http");
+const fs = require("fs");
+
+// Read File
+const data = fs.readFileSync("./data/data.json", "utf-8");
+const dataObj = JSON.parse(data);
+
+//create server
+const server = http.createServer((req, res) => {
+  res.end(data);
+});
+
+// start server
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Server running on port 8000");
+});
